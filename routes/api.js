88a5d29a04b93ab64c4be0b0a7d2b2d0ba56f9dd
Richard Marshall <rich.qazwsx@gmail.com>
2024-03-27 20:50:45 -0700

    feat: api error returns complete

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,24 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
+  const validLocales = ["american-to-british", "british-to-american"];
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    let { text, locale } = req.body;
+    const regex = /^\s*$/;
+    const str = req.body.text;
+    const isBlankOrWhiteSpace = regex.test(str);
+    if (isBlankOrWhiteSpace) {
+      return res.json({ error: "No text to translate" });
+    }
+    if (!text || !locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+    if (!validLocales.includes(locale)) {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+  });
 };
