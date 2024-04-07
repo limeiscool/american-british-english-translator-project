0aa1f76c58b1b90c09c103c7332695ad1c02b3a5
Richard Marshall <rich.qazwsx@gmail.com>
2024-04-07 01:40:33 -0700

    testing: unit tests written not passing

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -44,4 +44,44 @@ suite("Unit Tests", () => {
     );
     done();
   });
+
+  test("Translate 'The parking lot was full.' to British English", (done) => {});
+
+  test("Translate 'Like a high tech Rube Goldberg machine.' to British English", (done) => {});
+
+  test("Translate 'To play hooky means to skip class or work.' to British English", (done) => {});
+
+  test("Translate 'No Mr. Bond, I expect you to die.' to British Englis", (done) => {});
+
+  test("Translate 'Dr. Grosh will see you now.' to British English", (done) => {});
+
+  test("Translate 'Lunch is at 12:15 today.' to British English", (done) => {});
+
+  test("Translate 'We watched the footie match for a while.' to American English", (done) => {});
+
+  test("Translate 'Paracetamol takes up to an hour to work.' to American English", (done) => {});
+
+  test("Translate 'First, caramelise the onions.' to American English", (done) => {});
+
+  test("Translate 'I spent the bank holiday at the funfair.' to American English", (done) => {});
+
+  test("Translate 'I had a bicky then went to the chippy.' to American English", (done) => {});
+
+  test("Translate 'I've just got bits and bobs in my bum bag.' to American English", (done) => {});
+
+  test("Translate 'The car boot sale at Boxted Airfield was called off.' to American English", (done) => {});
+
+  test("Translate 'Have you met Mrs Kalyani?' to American English", (done) => {});
+
+  test("Translate 'Prof Joyner of King's College, London.' to American English", (done) => {});
+
+  test("Translate 'Tea time is usually around 4 or 4.30.' to American English", (done) => {});
+
+  test("Highlight translation in 'Mangoes are my favorite fruit.'", (done) => {});
+
+  test("Highlight translation in 'I ate yogurt for breakfast.'", (done) => {});
+
+  test("Highlight translation in 'We watched the footie match for a while.'", (done) => {});
+
+  test("Highlight translation in 'Paracetamol takes up to an hour to work.'", (done) => {});
 });
